63e2d2125264b947d826565df4c290b91a1c4149
TDgithub1 <105603327+TDgithub1@users.noreply.github.com>
2023-07-16 18:48:12 +0530

    1 file modified

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const Contact = () => <div>This is Contact</div>;
+const Contact = () => {
+  const [mainData, setMainData] = useState([]);
+
+  useEffect(() => {
+    const dataFetching = async () => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+
+      const data = await res.json();
+
+      if (data) {
+        setMainData(data);
+      }
+    };
+
+    dataFetching();
+  }, []);
+
+  return (
+    <div>
+      This is Contact
+      <div>
+        {mainData?.map(({ id, title }) => (
+          <Link to={`/contact/${id}`}>
+            <div>
+              <h3>{title}</h3>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
